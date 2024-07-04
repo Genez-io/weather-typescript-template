768d45bc83b371a3b3dcc8e2b4dca01223c46264
Radu Dumitrescu <d.raduandrei@gmail.com>
2024-07-04 11:15:26 +0300

    update template

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import genezioLocalSDKReload from "@genezio/vite-plugin-genezio";
+import react from "@vitejs/plugin-react"; //removed swc
 
 // https://vitejs.dev/config/
 export default defineConfig({
